591566044dbce22c473d54e6e42dcbeadd84f5f8
beruke <beeRoc@gmail.com>
2013-06-04 23:42:57 -0400

    Added mockup UI and JavaFX library.

---
+++ src/pcm/gui/UIMockupController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pcm.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author beeRoc
+ */
+public class UIMockupController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
