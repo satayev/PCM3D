8af01855588fb31f38a1ec90131771562859a7bc
Susan Storey <sstorey6@gatech.edu>
2013-07-23 18:46:20 -0400

    Changed data pane view and and line graph

--- src/pcm/gui/UIMockupController.java
+++ src/pcm/gui/UIMockupController.java
@@ -5,6 +5,7 @@ import dev.simple.SimpleFixedModel;
 import dev.simple.Tower;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -12,11 +13,13 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 
 import pcm.gui.graphics.*;
 
@@ -27,7 +30,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import pcm.model.geom.Vector;
@@ -39,19 +44,30 @@ public class UIMockupController implements Initializable {
   public Button csReset, csAdd;
   public ShapePane csPane;
   public FlowPane shapePicker;
-  public TextField csEdgeCount, shapeRotation, shapeScale;//, shapeHeight;
+  public TextField csEdgeCount, shapeRotation, shapeScale;//, shapeHeight; 
   private CSNode head;
-  public Tab simulationTab;
+  public Tab simulationTab, dataTab;
   public Button animationButton;
   public Text simulationResults;
   public TextField zenithField, azimuthField, latitudeField, longitudeField;
-  public CheckBox toEquatorCheckBox;
+  public CheckBox orbitCheckBox, toEquatorCheckBox;
   public Slider degreesSlider, photonsSlider, modelSizeSlider;
   public Label degreesLabel, photonsLabel, modelSizeLabel;
   public Accordion simulationAccordion1, simulationAccordion2, simulationAccordion3;
   public MenuBar dataMenuBar;
-    public Menu clearButton;
-  public GridPane dataGridPane;
+  public Menu clearButton; 
+  public GridPane dataOptionsGrid, dataGraphGrid;
+  public TitledPane userEarthOptions;
+
+  /** These variables are set according to the menu options selected */
+  List<Double> x;
+  List<Double> y;
+  String title;
+  String xLabel;
+  String yLabel;
+  LineChartGraph lcg = new LineChartGraph();
+  ListView<String> fileList = new ListView<String>(), modeList = new ListView<String>(), xAxisList = new ListView<String>(), yAxisList = new ListView<String>();
+  ObservableList<String> items;
   
   @Override
   public void initialize(URL url, ResourceBundle rb) {
@@ -62,115 +78,163 @@ public class UIMockupController implements Initializable {
     initializePatternTab();
     initializeSimulationTab();
     initializeDataTab();
-}
-
-private void initializeDataTab() {
-  assert dataAnchorPane != null : "fx:id=\"dataAnchorPane\" was not injected: check your FXML file 'UIMockup.fxml'.";
-  
-  assert dataMenuBar != null : "fx:id=\"dataMenuBar\" was not injected: check your FXML file 'UIMockup.fxml'.";
- 
-  final Menu menuMode = new Menu("Mode");
-  Menu menuXAxis = new Menu("X Axis");
-  Menu menuYAxis = new Menu("Y Axis");
-  dataMenuBar.getMenus().addAll(menuMode, menuXAxis, menuYAxis);
-
-  final String[] modeOptions = new String[] {"Wavelength", "Frequency"};
-  final RadioMenuItem[] modeButtons = new RadioMenuItem[modeOptions.length];
-  final ToggleGroup groupMode = new ToggleGroup();
-  for (int i=0;i<modeOptions.length;i++) {
-    modeButtons[i]  = new RadioMenuItem(modeOptions[i]);
-    modeButtons[i].setToggleGroup(groupMode);
-    menuMode.getItems().add(modeButtons[i]);
   }
-  groupMode.selectToggle(modeButtons[0]);
-  // Processing wavelength/frequency selection
-  groupMode.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-     public void changed(ObservableValue<? extends Toggle> ov,
-         Toggle old_toggle, Toggle new_toggle) {
-             if (groupMode.getSelectedToggle() != null) {
-               // Set as wavelength here
-               if (new_toggle == modeButtons[0]) {
-                 System.out.println(modeOptions[0]);
-               }
-               // Set as frequency here
-               if (new_toggle == modeButtons[1]) {
-                 System.out.println(modeOptions[1]);
-               }
-             }
-         }
-  });
-  
-  final String[] axisOptions = new String[] {"..", "..."};
-  final RadioMenuItem[] xAxisButtons = new RadioMenuItem[axisOptions.length];
-  final ToggleGroup groupXAxis = new ToggleGroup();
-  for (int i=0;i<axisOptions.length;i++) {
-    xAxisButtons[i]  = new RadioMenuItem(axisOptions[i]);
-    xAxisButtons[i].setToggleGroup(groupXAxis);
-    menuXAxis.getItems().add(xAxisButtons[i]);
-  }
-  groupXAxis.selectToggle(xAxisButtons[0]);
-  // Processing X-axis selection
-  groupXAxis.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-     public void changed(ObservableValue<? extends Toggle> ov,
-         Toggle old_toggle, Toggle new_toggle) {
-             if (groupMode.getSelectedToggle() != null) {
-               // Set as ..
-               if (new_toggle == xAxisButtons[0]) {
-                 System.out.println("x-axis set as"+ modeOptions[0]);
-               }
-               // Set as ...
-               else if (new_toggle == xAxisButtons[1]) {
-                 System.out.println("x-axis set as"+ modeOptions[1]);
-               }
-             }
-         }
-  });  
-  
-  final RadioMenuItem[] yAxisButtons = new RadioMenuItem[axisOptions.length];
-  final ToggleGroup groupYAxis = new ToggleGroup();
-  for (int i=0;i<axisOptions.length;i++) {
-    yAxisButtons[i]  = new RadioMenuItem(axisOptions[i]);
-    yAxisButtons[i].setToggleGroup(groupYAxis);
-    menuYAxis.getItems().add(yAxisButtons[i]);
-  }
-  groupYAxis.selectToggle(yAxisButtons[1]);
-  // Processing Y-axis selection
-  groupYAxis.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-     public void changed(ObservableValue<? extends Toggle> ov,
-         Toggle old_toggle, Toggle new_toggle) {
-             if (groupMode.getSelectedToggle() != null) {
-               // Set as ..
-               if (new_toggle == yAxisButtons[0]) {
-                 System.out.println("y-axis set as"+ modeOptions[0]);
-               }
-               // Set as ...
-               else if (new_toggle == yAxisButtons[1]) {
-                 System.out.println("y-axis set as"+ modeOptions[1]);
-               }
-             }
-         }
-  });
-  
-  // Adds the graph to Data tab
-  assert dataGridPane != null : "fx:id=\"dataGridPane\" was not injected: check your FXML file 'UIMockup.fxml'.";
-  
-  /** These variables are set according to the menu options selected */
-  // TODO need to be initialized
-  /*
-  List<Double> x;
-  List<Double> y;
-  String title; 
-  String xLabel; 
-  String yLabel;
-  LineChartGraph lcs = new LineChartGraph(x, y, title, xLabel, yLabel);
-  lcs.make();
-  lcs.lineChart.setPrefSize(Main.width, Main.height - Main.offsetTop - 50);
-  dataGridPane.add(lcs.lineChart, 0, 0);
-*/
-  
-}
 
-private void initializeSimulationTab() {
+  private void initializeDataTab() {
+    // Adds the graph to Data tab
+    assert dataGraphGrid != null : "fx:id=\"dataGraphGrid\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    lcg.make();
+    lcg.lineChart.setPrefSize(800, 500);
+    dataGraphGrid.add(lcg.lineChart, 0, 0);
+
+    /* Data tab */
+    assert dataTab != null : "fx:id=\"dataTab\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    dataTab.setOnSelectionChanged(new EventHandler<Event>() {
+      @Override
+      public void handle(Event t) {
+        if (dataTab.isSelected()) {
+          // Retrieve data from AppletModel/Interfacer
+          // TODO - John initialize vars
+          x = new ArrayList<Double>(Arrays.asList(new Double(0), new Double(3), new Double(9)));
+          y = new ArrayList<Double>(Arrays.asList(new Double(-1), new Double(8), new Double(9)));
+          title = "Test";
+          xLabel = "x-axis";
+          yLabel = "y-axis";
+
+          /* Default initial graph on data tab */
+          modeList.getSelectionModel().select(0);
+          xAxisList.getSelectionModel().select(0);
+          yAxisList.getSelectionModel().select(1);
+          
+        }
+      }
+    });
+    
+    /* Data tab options */
+    assert dataOptionsGrid != null : "fx:id=\"dataOptionsGrid\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    dataOptionsGrid.setHgap(5);
+    dataOptionsGrid.setVgap(5);
+    dataOptionsGrid.setPadding(new Insets(5, 5, 5, 5));
+
+    final String[] options = new String[] { "File", "Mode", "X-Axis", "Y-Axis" };
+    Text category;
+    for (int i = 0; i < options.length; i++) {
+      category = new Text(options[i]);
+      category.setFont(Font.font("Verdana", 11));
+      category.setFill(Color.WHITE);
+      dataOptionsGrid.add(category, i, 0);
+    }
+
+    final String[] fileOptions = new String[] { "Save graph as .csv" };
+    final String[] modeOptions = new String[] { "Wavelength", "Frequency" };
+    final String[] axisOptions = new String[] { "..", "..." };
+
+    /* Creating lists */
+    items = FXCollections.observableArrayList(fileOptions);
+    fileList.setItems(items);
+    dataOptionsGrid.add(fileList, 0, 1);
+    items = FXCollections.observableArrayList(modeOptions);
+    modeList.setItems(items);
+    dataOptionsGrid.add(modeList, 1, 1);
+    items = FXCollections.observableArrayList(axisOptions);
+    xAxisList.setItems(items);
+    dataOptionsGrid.add(xAxisList, 2, 1);
+    items = FXCollections.observableArrayList(axisOptions);
+    yAxisList.setItems(items);
+    dataOptionsGrid.add(yAxisList, 3, 1);
+    
+    HBox hbox = new HBox(); // space holder for layout
+    hbox.setPrefWidth(400);
+    dataOptionsGrid.add(hbox, 4, 1);
+    
+    /* User option listeners */
+    fileList.getSelectionModel().selectedItemProperty().addListener(
+        new ChangeListener<String>() {
+          public void changed(ObservableValue<? extends String> ov,
+              String old_val, String new_val) {
+            if (new_val.equals(fileOptions[0])) {
+              // Save .cvs
+            }
+          }
+        });
+    // TODO - John for the next three listeners
+    modeList.getSelectionModel().selectedItemProperty().addListener(
+        new ChangeListener<String>() {
+          public void changed(ObservableValue<? extends String> ov,
+              String old_val, String new_val) {
+            if (new_val.equals(modeOptions[0])) {
+              // Wavelength
+              //change affected vars
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+            else if (new_val.equals(modeOptions[1])) {
+              // Frequency
+              //change affected vars
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+          }
+        });
+
+    xAxisList.getSelectionModel().selectedItemProperty().addListener(
+        new ChangeListener<String>() {
+          public void changed(ObservableValue<? extends String> ov,
+              String old_val, String new_val) {
+            if (new_val.equals(axisOptions[0])) {
+              //x = ....;
+              xLabel = axisOptions[0];
+              title = yLabel + " vs " + xLabel;
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+            else if (new_val.equals(axisOptions[1])) {
+              //x = ....;
+              xLabel = axisOptions[1];
+              title = yLabel + " vs " + xLabel;
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+          }
+        });
+
+    yAxisList.getSelectionModel().selectedItemProperty().addListener(
+        new ChangeListener<String>() {
+          public void changed(ObservableValue<? extends String> ov,
+              String old_val, String new_val) {
+            if (new_val.equals(axisOptions[0])) {
+              y = new ArrayList<Double>(Arrays.asList(new Double(8), new Double(3), new Double(0)));;
+              yLabel = axisOptions[0];
+              title = yLabel + " vs " + xLabel;
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+            else if (new_val.equals(axisOptions[1])) {
+              //y = ....;
+              yLabel = axisOptions[1];
+              title = yLabel + " vs " + xLabel;
+              lcg.update(x, y, title, xLabel, yLabel);
+            }
+          }
+        });
+
+
+
+  }
+
+  private void initializeSimulationTab() {
+    /*
+     * Simulation tab controls using AppletInterfacer
+     */
+    assert simulationTab != null : "fx:id=\"simulationTab\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    simulationTab.setOnSelectionChanged(new EventHandler<Event>() {
+      @Override
+      public void handle(Event t) {
+        if (simulationTab.isSelected()) {
+//          if (shapeCanvas.getChildren().size() > 0)
+            AppletInterfacer.setModel(1, 1, 1, makeEdgeLists());
+          AppletInterfacer.open();
+        } else {
+          AppletInterfacer.standBy();
+        }
+      }
+    });
+
     assert simulationAccordion1 != null : "fx:id=\"simulationAccordion1\" was not injected: check your FXML file 'UIMockup.fxml'.";
     simulationAccordion1.expandedPaneProperty().addListener(new ChangeListener<TitledPane>() {
       @Override
@@ -199,22 +263,6 @@ private void initializeSimulationTab() {
           simulationAccordion3.setMaxHeight(500);
         else
           simulationAccordion3.setMaxHeight(0);
-      }
-    });
-    /*
-     * Simulation tab controls using AppletInterfacer
-     */
-    assert simulationTab != null : "fx:id=\"simulationTab\" was not injected: check your FXML file 'UIMockup.fxml'.";
-    simulationTab.setOnSelectionChanged(new EventHandler<Event>() {
-      @Override
-      public void handle(Event t) {
-        if (simulationTab.isSelected()) {
-//        if (shapeCanvas.getChildren().size() > 0)
-            AppletInterfacer.setModel(1, 1, 1, makeEdgeLists());
-          AppletInterfacer.open();
-        } else {
-          AppletInterfacer.standBy();
-        }
       }
     });
 
@@ -255,6 +303,23 @@ private void initializeSimulationTab() {
     fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
     fiveSecondsWonder.play();
 
+    assert userEarthOptions != null : "fx:id=\"userEarthOptions\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    assert orbitCheckBox != null : "fx:id=\"orbitCheckBox\" was not injected: check your FXML file 'UIMockup.fxml'.";
+    // Whether to use preset ISS orbit or not
+    orbitCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+      @Override
+      public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        if (newValue) {
+          userEarthOptions.setPrefHeight(45);
+          //TODO - John
+        } else {
+          userEarthOptions.setPrefHeight(90);
+          //TODO - John
+        }
+      }
+    });
+    
+
     assert zenithField != null : "fx:id=\"zenithField\" was not injected: check your FXML file 'UIMockup.fxml'.";
     assert azimuthField != null : "fx:id=\"azimuthField\" was not injected: check your FXML file 'UIMockup.fxml'.";
     assert latitudeField != null : "fx:id=\"latitudeField\" was not injected: check your FXML file 'UIMockup.fxml'.";
@@ -264,7 +329,6 @@ private void initializeSimulationTab() {
     zenithField.setText(String.valueOf(AppletInterfacer.getZenith()));
     azimuthField.setText(String.valueOf(AppletInterfacer.getAzimuth()));
 
-    // TODO - Add ability for user to choose switch to radians?
     // TODO - Accept only valid values from text fields, prompt user when invalid
     zenithField.setOnKeyReleased(new EventHandler<KeyEvent>() {
       public void handle(KeyEvent ke) {
@@ -312,7 +376,7 @@ private void initializeSimulationTab() {
             shapeCanvas.getChildren().clear();
             clearButton.hide();
         }
-    });
+    }); 
     shapeRotation.textProperty().addListener(new ChangeListener<String>() {
       @Override
       public void changed(ObservableValue<? extends String> ov, String t, String t1) {
@@ -421,7 +485,7 @@ private void initializeSimulationTab() {
       public void handle(DragEvent t) {
         if (!shapeCanvas.getChildren().contains(ShapePane.dragPreview)) {
           double scale = 0.1d;
-//          double height = 1.0d;
+//          double height = 1.0d; 
           try {
             scale = Double.parseDouble(shapeScale.getText()) * 600.0d;
           } catch (NumberFormatException e) {
@@ -431,8 +495,8 @@ private void initializeSimulationTab() {
 //            height = Double.parseDouble(shapeHeight.getText());
 //          } catch (NumberFormatException e) {
 //            System.out.println(e);
-//          }
-
+//          } 
+          
           ShapePane.dragPreview.scale(scale);
           shapeCanvas.getChildren().add(ShapePane.dragPreview);
         }
