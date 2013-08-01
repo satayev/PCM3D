096bedff8472385e40328bd1bb5a0d8bc76ffbf0
Susan Storey <sstorey6@gatech.edu>
2013-08-01 17:40:42 -0400

    Now everything GUI.jar needs is in lib folder - pcm.gui.main.jar, processing-1.5.1/dlls, processing-1.5.1/dlls64

--- src/pcm/gui/JarRunnable.java
+++ src/pcm/gui/JarRunnable.java
@@ -14,9 +14,9 @@ public class JarRunnable {
   public static void main(String[] args) throws IOException {
 	  Process proc;
 	  if (System.getProperty("sun.arch.data.model").equals("64"))
-		  proc = Runtime.getRuntime().exec("java -Xms256m -Xmx512m -Djava.library.path=src/data/dlls64 -jar pcm.gui.main.jar");
+		  proc = Runtime.getRuntime().exec("java -Xms256m -Xmx512m -Djava.library.path=lib/processing-1.5.1/dlls64 -jar lib/pcm.gui.main.jar");
 	  else 
-		  proc = Runtime.getRuntime().exec("java -Xms256m -Xmx512m -Djava.library.path=src/data/dlls -jar pcm.gui.main.jar");
+		  proc = Runtime.getRuntime().exec("java -Xms256m -Xmx512m -Djava.library.path=lib/processing-1.5.1/dlls -jar lib/pcm.gui.main.jar");
 	  
   }
 
