8c397c493a9fb6994123bb4043cabe28d72a655e
rsl3 <ricardo.saenz@gtri.gatech.edu>
2013-10-09 18:35:31 -0400

    Adding comment

--- src/pcm/model/statics/Alpha.java
+++ src/pcm/model/statics/Alpha.java
@@ -6,6 +6,7 @@ public class Alpha {
    * Calculate alpha
    * @param wavelength The wavelength of the photon
    * @return Alpha, in units cm-1
+   * Absorption Coefficient
    */
   public static double getAlpha(double wavelength) {
     return 4 * Math.PI * ExtinctionCoefficient.getK() / (wavelength *100);
